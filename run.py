f0ace8c539b8e60686b9403da432b50ece5bc524
steveforrest <steveforrest_1@hotmail.com>
2021-07-22 13:09:22 +0000

    add sales data to worksheet

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 
+
 import gspread
 from google.oauth2.service_account import Credentials
 # python code goes here
@@ -17,23 +18,21 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 def get_sales_data():
     """
     get sales figures from user
+    Run a while loop to collect a valid string of data from the user	
+    via the terminal, which must be a string of 6 numbers separated	
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
-
         print('Please eneter sales data from the last market.')
         print('Data should be six numbers, separated by commas.')
         print('Example: 10,20,30,40,50,60\n')
-
         data_str = input('Enter your data here: ')
-        
         sales_data = data_str.split(',')
         validate_data(sales_data)
-        
         if validate_data(sales_data):
             print('data is valid')
             break
-        
-
+    return sales_data
 
 def validate_data(values):
     """
@@ -52,4 +51,15 @@ def validate_data(values):
         return False
     return True
 
-get_sales_data()
+def update_sales_worksheet(data):
+    """
+    update sales worksheet, add new row withthe list data provided.
+    """
+    print('updating sales worksheet....\n')
+    sales_worksheet = SHEET.worksheet('sales')
+    sales_worksheet.append_row(data)
+    print('sales worksheet updated successfully\n')
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
