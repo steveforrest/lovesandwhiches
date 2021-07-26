164ac947135ed20507ba8a32e02b2542d9b65ee4
steveforrest <steveforrest_1@hotmail.com>
2021-07-26 12:44:38 +0000

    added requirements for heroku for deployment1

--- run.py
+++ run.py
@@ -25,7 +25,7 @@ def get_sales_data():
         print('Please eneter sales data from the last market.')
         print('Data should be six numbers, separated by commas.')
         print('Example: 10,20,30,40,50,60\n')
-        data_str = input('Enter your data here: ')
+        data_str = input('Enter your data here: \n')
         sales_data = data_str.split(',')
         validate_data(sales_data)
         if validate_data(sales_data):
